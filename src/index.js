a48e8bca87155a58814cd2eeb1e97549ceab7430
maegatro <shuntaromaekawa@gmail.com>
2020-06-03 20:15:25 +0900

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./styles.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createEditor } from "./editor";
+import debounce from "debounce";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// default code
+const code = `import x from 'x';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// edit this example
+
+function Greet() {
+  return <span>Hello World!</span>
+}
+
+<Greet />
+`;
+
+class SandBox extends React.Component {
+  state = {
+    code,
+  };
+
+  editor = null;
+
+  el = null;
+
+  componentDidMount() {
+    this.editor = createEditor(this.el);
+    this.editor.run(code);
+  }
+
+  onCodeChange = ({ target: { value } }) => {
+    this.setState({ code: value });
+    this.run(value);
+  };
+
+  run = debounce(() => {
+    const { code } = this.state;
+    this.editor.run(code);
+  }, 500);
+
+  render() {
+    const { code } = this.state;
+    return (
+      <div className="app">
+        <div className="split-view">
+          <div className="code-editor">
+            <textarea value={code} onChange={this.onCodeChange} />
+          </div>
+          <div className="preview" ref={(el) => (this.el = el)} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<SandBox />, rootElement);
